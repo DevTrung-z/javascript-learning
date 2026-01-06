e5f7de452354df1cb64791400fcd0dfb7d23d5f1
DevTrung-z <devtrung1406@gmail.com>
2026-01-06 22:05:13 +0700

    Hoàng thành Variable JS

--- 01-javascript-variables/variables.js
+++ 01-javascript-variables/variables.js
@@ -1,5 +1,31 @@
-let myName = "Ngyen Thanh Trung";
+// Bài 1
+// Khai báo biến:
+
+// fullName
+// age
+// isStudent
+// In ra console:
+
+// Tên: ...
+// Tuổi: ...
+// Sinh viên: ...
+
+// 📌 Yêu cầu:
+// fullName: let
+// age: let
+// isStudent: const
+
+let fullName = "Trung";
+let age = 20;
+const isStudent = true;
+
+console.log(fullName);
+console.log(age);
+console.log(isStudent);
 
 const heading = document.querySelector("#heading_A");
-heading.textContent = myName;
-    
+heading.textContent = fullName + age + isStudent;
+
+
+
+
